31e6c7564e140452df908211daf7658bbece59e6
Mateusz <matekudr@gmail.com>
2023-11-20 22:49:41 +0100

    Create Data folder Write connection string Add services

---
+++ BulkyWeb/Data/ApplicationDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace BulkyWeb.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) 
+        {
+                
+        }
+    }
+}
