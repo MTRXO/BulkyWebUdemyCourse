16861fa1aa8446987a1594d8e5acf932072a295a
Mateusz <matekudr@gmail.com>
2024-02-29 22:23:30 +0100

    inplement IReository

---
+++ Bulky.DataAccess/IRepository/Repository.cs
@@ -0,0 +1,54 @@
+﻿using Bulky.DataAcces.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+
+namespace Bulky.DataAccess.IRepository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDBContext _db;
+        internal DbSet<T> _dbSet;   
+        public Repository(ApplicationDBContext db)
+        {
+           _db = db;
+           this._dbSet = _db.Set<T>();  
+        }
+
+        public void Add(T entity)
+        {
+           _dbSet.Add(entity);  
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = _dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+
+
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = _dbSet;
+            return query.ToList();  
+         }
+
+        public void Remove(T entity)
+        {
+
+             _dbSet.Remove(entity); 
+        }
+
+        public void Remove(IEnumerable<T> entity)
+        {
+            _db.RemoveRange(entity);
+        }
+    }
+}
