f84ec5122b8b7ee01bbbfcead29d26e3f87b47ba
Mateusz <matekudr@gmail.com>
2024-05-21 00:27:33 +0200

    Create IUnitOfWork

---
+++ Bulky.DataAccess/Repository/IRepository/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using Bulky.DataAcces.Data;
+using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    internal class UnitOfWork: IUnitOfWork
+    {
+        private readonly ApplicationDBContext _db;
+        public ICategoryRepository Category { get; set; }
+
+        public UnitOfWork(ApplicationDBContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();    
+        }
+    }
+}
